a89849bfa035f1dd112d1418b8d9907b75ae930a
hvnguyen <hoang.van.nguyen@mgm-tp.com>
2019-07-26 16:05:21 +0700

    OK

--- src/containers/demo/Button.js
+++ src/containers/demo/Button.js
@@ -3,33 +3,31 @@ import { Button, Icon } from '../../components/core';
 
 const ButtonDemo = () => {
   return (
-    <div className="p-10 text-white">
-      <div className="container flex items-center mx-auto mb-1">
-        <Button color="teal-500" className="mr-1">Click</Button>
-        <Button color="teal-500" className="mr-1"><Icon name="heart" color="red-500" /></Button>
-        <Button color="purple-500" className=" mr-1">
-          <Icon name="heart" color="red-500" className="mr-2" />
-          Click
-        </Button>
-        <Button color="red-500" className="mr-1">
-          <Icon name="heart" className="mr-2" />
-          Click
-        </Button>
-        <Button color="blue-500" className="mr-1">
-          Click
-          <Icon name="repeat" className="ml-2" />
-        </Button>
-        <Button color="blue-600" className="mr-1">
-          <Icon name="heart" className="mr-2" />
-          Click
-          <Icon name="repeat" className="ml-2" />
-        </Button>
-        <Button color="blue-300" className="mr-1">
-          <Icon name="heart" className="mr-2" />
-          Click
-          <Icon name="repeat" className="ml-2" />
-        </Button>
-      </div>
+    <div className="container flex items-center mx-auto mb-1">
+      <Button color="teal-500" className="mr-1">Click</Button>
+      <Button color="teal-500" className="mr-1"><Icon name="heart" color="red-500" /></Button>
+      <Button color="purple-500" className=" mr-1">
+        <Icon name="heart" color="red-500" className="mr-2" />
+        Click
+      </Button>
+      <Button color="red-500" className="mr-1">
+        <Icon name="heart" className="mr-2" />
+        Click
+      </Button>
+      <Button color="blue-500" className="mr-1">
+        Click
+        <Icon name="repeat" className="ml-2" />
+      </Button>
+      <Button color="blue-600" className="mr-1">
+        <Icon name="heart" className="mr-2" />
+        Click
+        <Icon name="repeat" className="ml-2" />
+      </Button>
+      <Button color="blue-300" className="mr-1">
+        <Icon name="heart" className="mr-2" />
+        Click
+        <Icon name="repeat" className="ml-2" />
+      </Button>
     </div>
   );
 };
