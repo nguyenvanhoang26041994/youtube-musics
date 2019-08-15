0ab1b8f212667c7143210eed9b207ae269476e78
hvnguyen <hoang.van.nguyen@mgm-tp.com>
2019-08-15 13:52:09 +0700

    OK

--- src/components/Playlist/MusicItem.js
+++ src/components/Playlist/MusicItem.js
@@ -29,12 +29,12 @@ const MusicItemWrapper = styled.div`
 const MusicItem = ({ className, onClick, name, singersName, time, listenNumber, isActive, isPlaying, index }) => {
   return (
     <MusicItemWrapper className={cn('ui-music-item flex h-8 items-center hover:glass cursor-pointer transition-fast', { 'ui-music-item--active': isActive }, className)} onClick={onClick}>
-      <div className="flex items-center pl-3">
-        <div className="text-white text-2xs font-mono w-12">{index}</div>
-        <h5 className="ui-music-item__name cursor-pointer w-72 text-white text-sm">{name}</h5>
-        <h5 className="cursor-pointer w-64 text-xs text-gray-400">{singersName}</h5>
+      <div className="flex items-center pl-3 w-2/3 h-full">
+        <div className="text-white text-2xs font-mono w-1/12">{index}</div>
+        <h5 className="ui-music-item__name cursor-pointer w-6/12 text-white text-sm">{name}</h5>
+        <h5 className="cursor-pointer w-5/12 text-xs text-gray-400">{singersName}</h5>
       </div>
-      <div className="flex items-center flex-1 justify-end pr-3">
+      <div className="flex items-center flex-1 justify-end pr-3 w-1/3 h-full">
         <div className="ui-music-item__options flex items-center flex-1 justify-end mr-5">
           <Icon name="download" size="sm" color="white" className="mx-2" />
           <Icon name="heart" size="sm" color="white" className="mx-2" />
