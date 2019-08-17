e45dde45625350937db8f4a0ca9a989c3adea976
hvnguyen <hoang.van.nguyen@mgm-tp.com>
2019-08-17 17:35:27 +0700

    OK

--- src/layouts/Navbar/Navbar.js
+++ src/layouts/Navbar/Navbar.js
@@ -3,10 +3,44 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
-import { Logo } from '../../components/core';
+import { Logo, Switch } from '../../components/core';
 
 const NavbarWrapper = styled.nav`
 `;
+
+const onFullScreen = e => {
+  const documentElement = document.documentElement;
+
+  try {
+    if (e.target.checked) {
+      if (documentElement.requestFullscreen) {
+        return documentElement.requestFullscreen();
+      }
+
+      if (documentElement.mozRequestFullScreen) {
+        return documentElement.mozRequestFullScreen();
+      }
+
+      if (documentElement.msRequestFullscreen) {
+        return documentElement.msRequestFullscreen();
+      }
+    }
+
+    if (document.exitFullscreen) {
+      return document.exitFullscreen();
+    }
+
+    if (document.webkitExitFullscreen) {
+      return document.webkitExitFullscreen();
+    }
+
+    if (document.msExitFullscreen) {
+      return document.msExitFullscreen();
+    }
+  } catch (e) {
+    // DO SOME THING
+  }
+};
 
 const Navbar = ({ className }) => (
   <NavbarWrapper className={cn('ui-navbar relative overflow-hidden flex justify-center text-white w-full h-16 bg-lizard-gradient', className)}>
@@ -17,6 +51,13 @@ const Navbar = ({ className }) => (
             <a><Logo className="w-8 h-8" /></a>
           </Link>
         </div>
+      </div>
+      <div>
+        <Switch
+          color="teal-400"
+          size="sm"
+          onChange={onFullScreen}
+        />
       </div>
     </div>
   </NavbarWrapper>
