8230f8590645918bd2da3e3f523e4a6242f40c2b
hvnguyen <hoang.van.nguyen@mgm-tp.com>
2019-08-20 07:49:22 +0700

    Change size font

--- src/components/MusicWithLyric/MusicWithLyric.js
+++ src/components/MusicWithLyric/MusicWithLyric.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useDebugValue } from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -25,17 +25,17 @@ const MusicWithLyric = ({ className, music, lyric, playMusic }) => {
   return (
     <MusicWithLyricWrapper className={cn('ui-music-with-lyric flex flex-col w-full relative', className)} isCollapsed={isCollapsed}>
       <div className="ui-music-with-lyric__name w-full flex justify-center sticky z-10">
-        <Button size="xl" color="teal-400" className={cn('m-2 text-white rounded-full')} onClick={playMusic}>
-          <div className="text-lg flex items-center mx-10">
+        <Button size="base" color="teal-400" className={cn('m-2 text-white rounded-full')} onClick={playMusic}>
+          <div className="text-base flex items-center mx-10">
             <Icon name="music-note" className="mr-3" />
-            <span className="font-lovers-quarrel text-3xl">{music.singersName} - {music.name}</span>
+            <span className="font-lovers-quarrel text-xl">{music.singersName} - {music.name}</span>
             <Icon name="music-note" className="ml-3" />
           </div>
         </Button>
       </div>
       {lyric && lyric.data && lyric.data.length && (
         <div className="flex flex-col">
-          <ul className="ui-music-with-lyric__lyric-wrapper flex flex-col items-center font-shadows-into-light text-xl text-base overflow-hidden">
+          <ul className="ui-music-with-lyric__lyric-wrapper flex flex-col items-center font-shadows-into-light text-sm text-base overflow-hidden">
             {lyric && lyric.data.map((obj, idx) => (
               <li key={idx} className="text-gray-100">
                 <p className={cn('lyric-line transition-fast', { 'lyric-line--active text-teal-400': obj.timeStart < currentTime && obj.timeEnd > currentTime})}>{obj.text}</p>
