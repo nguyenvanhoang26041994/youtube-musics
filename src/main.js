9d66be4991be66bc2c5e46aa09da1a617cb72595
hvnguyen <hoang.van.nguyen@mgm-tp.com>
2019-09-05 12:56:52 +0700

    Refactor

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
 import dynamic from 'next/dynamic';
-import AppSSR from './App';
 
-let App;
-const mode = process.env.APP_MODE;
-
-if (mode === 'SPA') {
-  App = dynamic(() => import('./App'), { ssr: false });
-} else {
-  App = AppSSR;
-}
-
-export default App;
+export default dynamic(() => import('./App'), { ssr: !process.env.APP_MODE === 'SPA' });
