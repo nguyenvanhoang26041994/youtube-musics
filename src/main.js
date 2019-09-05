7ea278004de3419dce306a76c0813f13bf00d19e
hvnguyen <hoang.van.nguyen@mgm-tp.com>
2019-09-05 13:01:46 +0700

    Fix

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import dynamic from 'next/dynamic';
+import AppSSR from './App';
 
-export default dynamic(() => import('./App'), { ssr: !process.env.APP_MODE === 'SPA' });
+let App;
+const mode = process.env.APP_MODE;
+
+if (mode === 'SPA') {
+  App = dynamic(() => import('./App'), { ssr: false });
+} else {
+  App = AppSSR;
+}
+
+export default App;
